b9337298fc9f74476a5757420fe38460294626f4
Marc <PotthofMarc@gmail.com>
2025-09-01 22:18:16 +0200

    sehr tolle enderungen die besa sind alss die von maxim

--- src/App.java
+++ src/App.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class App {
+    private static final String RESET = "\u001B[0m";
+    private static final String CYAN = "\u001B[36m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String RED = "\u001B[31m";
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        printAsciiArt();
+        greetUser();
+        handleUserInput();
+        printMathJoke();
+    }
 
-        // ANSI-Farbcodes für bunte Ausgabe
-        final String RESET = "\u001B[0m";
-        final String CYAN = "\u001B[36m";
-        final String YELLOW = "\u001B[33m";
-        final String RED = "\u001B[31m";
-
-        // ASCII-Art
+    private static void printAsciiArt() {
         System.out.println(CYAN +
             "  __  __           _       _           _ " + "\n" +
             " |  \\/  | ___   __| |_   _| | ___  ___| |" + "\n" +
@@ -18,12 +21,21 @@ public class App {
             " | |  | | (_) | (_| | |_| | |  __/\\__ \\_|" + "\n" +
             " |_|  |_|\\___/ \\__,_|\\__,_|_|\\___||___(_)" + RESET
         );
+    }
 
+    private static void greetUser() {
         System.out.println(YELLOW + "Hallo, Maxim!" + RESET);
-        System.out.print("Gib etwas ein: ");
-        String Dings = s.nextLine();
-        System.out.println(RED + Dings + " is doof!" + RESET);
+    }
 
-        s.close();
+    private static void handleUserInput() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Gib etwas ein: ");
+            String input = scanner.nextLine();
+            System.out.println(RED + input + " is doof!" + RESET);
+        }
+    }
+
+    private static void printMathJoke() {
+        System.out.println("2 + 2 = 5");
     }
 }
